17ba6f64ae30c5c7d675f2d902efe7a07a69abf0
Akash Sahu <akashsahu006@gmail.com>
2022-06-21 15:52:37 +0530

    Project initialization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' bg-gradient-to-r from-cyan-500 to-blue-500 h-screen w-screen flex items-center justify-center flex-col'>
+      <h1 className='text-white text-3xl font-antic font-semibold tracking-widest'>
+        Priceify
+      </h1>
+      <h2 className='my-4 font-semibold text-white'>
+        Website is under Construction
+      </h2>
+    </div>
+  )
+}
+
+export default Home
